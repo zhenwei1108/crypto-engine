87af59cf8b90ca1923cecad073245a1d9eb4ea0e
zhenwei1108 <zhenwei1108@163.com>
2024-01-22 23:14:02 +0800

    add attribute

--- pkcs/base.go
+++ pkcs/base.go
@@ -9,3 +9,8 @@ type IssuerAndSerialNumber struct {
 	x500Name         asn1.RawValue "X500Name, 用于描述DN"
 	certSerialNumber *big.Int
 }
+
+type attribute struct {
+	attributeType  asn1.ObjectIdentifier
+	attributeValue asn1.RawValue
+}
