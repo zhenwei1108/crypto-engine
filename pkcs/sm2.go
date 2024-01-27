b3b3d12bfb517fcc7e48a22bb5de8a724b19719e
wegoo <zhenwei1108@163.com>
2024-01-27 23:38:52 +0800

    补充SM2部分结构定义

--- pkcs/sm2.go
+++ pkcs/sm2.go
@@ -2,15 +2,16 @@ package pkcs
 
 import "math/big"
 
+// SM2签名结构
 type SM2Signature struct {
-	r big.Int
-	s big.Int
+	R *big.Int
+	S *big.Int
 }
 
-// C1 C3 C2
+// SM2密文结构 C1 C3 C2
 type SM2Cipher struct {
-	x          big.Int "x分量"
-	y          big.Int "y分量"
-	hash       []byte  "size 32"
-	cipherText []byte  "密文"
+	X          *big.Int "x分量"
+	Y          *big.Int "y分量"
+	Hash       []byte   "size 32"
+	CipherText []byte   "密文"
 }
