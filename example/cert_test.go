01aa453008af22dedd0b477783f0014047c53a46
wegoo <zhenwei1108@163.com>
2024-01-28 00:03:20 +0800

    gg

---
+++ example/cert_test.go
@@ -0,0 +1,57 @@
+package example
+
+import (
+	"crypto-engine/pkcs"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"math/big"
+	"testing"
+	"time"
+)
+
+func Test_code_cert(t *testing.T) {
+	var cert = "MIIB8zCCAZmgAwIBAgIKAKjKSNY55JbBcTAKBggqhkjOPQQDAjBCMSowKAYDVQQDDCFNaWRlYSBHcm91cCBNYXR0ZXIgUEFJIEcxIEkxIFByb2QxFDASBgorBgEEAYKifAIBEwQxMThDMCAXDTIzMTAyNDE1NTM0MVoYDzIwODcwODI0MTU1OTU5WjBXMSkwJwYDVQQDDCBEOUIyQUFFODcxQjUzRUU2NTYzMjQyOTYxNDUxRUJGRjEUMBIGCisGAQQBgqJ8AgETBDExOEMxFDASBgorBgEEAYKifAICEwQyMDIyMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEzFklDsljVKzMwNtBMBHLsx+psYLFYPMiGKKOTq2ur0S1SpcPQTmg0kUnu9X6RfJSnW7qBUJQDsPYXTwRifkv+qNgMF4wDAYDVR0TAQH/BAIwADAOBgNVHQ8BAf8EBAMCB4AwHQYDVR0OBBYEFFLhCG3CvDaY1Mu1QtyX2W783wfvMB8GA1UdIwQYMBaAFFm1h6KGoZDqlB20oJiuFdaAAxF4MAoGCCqGSM49BAMCA0gAMEUCIHdFM1UodAqZ73jmDrt786QEn3+jZ0lA7KZy7RzpBJiDAiEA7PRZ0D3NJfq7kKchqww8fDkWGIPwSZ6t5ADlwyuWC5g="
+	decodeBytes, err := base64.StdEncoding.DecodeString(cert)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	certResult, err := asn1.Unmarshal(decodeBytes, &pkcs.Certificate{})
+	fmt.Println(certResult)
+	fmt.Println(err)
+
+}
+
+// todo go原生中包含cert结构 gg
+func Test_create_cert(t *testing.T) {
+	//sm2
+	alg := pkix.AlgorithmIdentifier{Algorithm: asn1.ObjectIdentifier{1, 2, 156, 10197, 1, 301, 1}}
+	dn := pkix.Name{Country: []string{"CN"}, CommonName: "adsf"}
+	notBefore := time.Now()
+	validity := pkcs.Validity{notBefore, notBefore.AddDate(2, 0, 0)}
+	pub := make([]byte, 65)
+	info := pkcs.SubjectPublicKeyInfo{pkix.AlgorithmIdentifier{asn1.ObjectIdentifier{1, 2, 156, 10197, 1, 301, 1}, asn1.NullRawValue}, pub}
+	//设置版本
+	realVersion, _ := asn1.Marshal(2)
+	version := asn1.RawValue{
+		Class:      asn1.ClassContextSpecific,
+		Tag:        0,
+		Bytes:      realVersion,
+		IsCompound: true,
+	}
+	signAlg := pkix.AlgorithmIdentifier{Algorithm: asn1.ObjectIdentifier{1, 2, 156, 10197, 1, 501}}
+	tbs := pkcs.TBSCertificate{Version: version, SerialNumber: big.NewInt(123456), Signature: signAlg, Issuer: dn, Validity: validity, Subject: dn, SubjectPublicKeyInfo: info}
+
+	//SM2Signature转为sequence
+	signature := pkcs.SM2Signature{R: big.NewInt(1234567890), S: big.NewInt(987654321)}
+	signatureData, _ := asn1.Marshal(signature)
+
+	certificate := pkcs.Certificate{tbs, alg, signatureData}
+	fmt.Println(certificate)
+	hexCert, err := asn1.Marshal(certificate)
+	fmt.Println(err)
+	fmt.Println(hex.EncodeToString(hexCert))
+}
