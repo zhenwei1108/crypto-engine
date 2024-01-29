3c6b7a6057b931ba0f0f6b45ecd53e1ce3e8d198
zhenwei1108 <zhenwei1108@163.com>
2024-01-29 23:05:19 +0800

    完善证书结构定义

--- pkcs/certificate.go
+++ pkcs/certificate.go
@@ -10,7 +10,7 @@ import (
 // GBT 20518-2018 信息安全技术 公钥基础设施 数字证书格式
 // GMT-0015
 type Certificate struct {
-	TbsCertificate     TBSCertificate `struct:"TBSCertificate"`
+	TbsCertificate     TBSCertificate
 	SignatureAlgorithm pkix.AlgorithmIdentifier
 	SignatureValue     []byte `struct:"SM2Signature/[]byte" desc:"签名值"`
 }
@@ -20,24 +20,16 @@ type TBSCertificate struct {
 	SerialNumber         *big.Int
 	Signature            pkix.AlgorithmIdentifier //签名算法
 	Issuer               pkix.Name
-	Validity             Validity `struct:"Validity"`
+	Validity             Validity
 	Subject              pkix.Name
 	SubjectPublicKeyInfo SubjectPublicKeyInfo `struct:"SubjectPublicKeyInfo"` //公钥信息
-	IssuerUniqueID       asn1.RawValue        `tag:"1" optional:"true" desc:"如果出现，版本选择V2或V3"`
-	SubjectUniqueID      asn1.RawValue        `tag:"2" optional:"true" desc:"如果出现，版本选择V2或V3"`
-	// 为什么不可以 []Extension
-	Extensions []Extension `struct:"Extension" tag:"3"  optional:"true" desc:"如果出现，版本选择V3"`
+	IssuerUniqueID       asn1.RawValue        `asn1:"optional,tag:1"`         // omitempty 指定此基础字段不被编码，   asn1:"optional“表示asn1不编码
+	SubjectUniqueID      asn1.RawValue        `asn1:"optional,tag:2" desc:"如果出现，版本选择V3"`
+	Extensions           []Extension          `asn1:"optional,tag:2" desc:"如果出现，版本选择V3"`
 }
 
 // 有效期
 type Validity struct {
 	NotBefore time.Time
 	NotAfter  time.Time
-}
-
-// 扩展项
-type Extension struct {
-	ExtnID    asn1.ObjectIdentifier
-	Critical  bool
-	ExtnValue []byte
 }
