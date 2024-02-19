76311af78613ca8a55d8911239269d6e7598938b
wegoo <zhenwei1108@163.com>
2024-02-19 22:21:46 +0800

    补充SM2相关结构

--- pkcs/sm2.go
+++ pkcs/sm2.go
@@ -1,6 +1,11 @@
 package pkcs
 
-import "math/big"
+import (
+	"crypto/elliptic"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"math/big"
+)
 
 // SM2签名结构
 type SM2Signature struct {
@@ -14,4 +19,23 @@ type SM2Cipher struct {
 	Y          *big.Int "y分量"
 	Hash       []byte   "size 32"
 	CipherText []byte   "密文"
+}
+
+type SM2PublicKey struct {
+	elliptic.Curve
+	X *big.Int
+	Y *big.Int
+}
+
+type SM2KeyPair struct {
+	PublicKey  SM2PublicKey
+	PrivateKey asn1.BitString
+}
+
+/**密钥保护结构*/
+type SM2EnvelopedKey struct {
+	SymAlgID               pkix.AlgorithmIdentifier
+	SymEncryptData         SM2Cipher
+	Sm2PublicKey           SM2PublicKey
+	Sm2EncryptedPrivateKey asn1.BitString
 }
