cff0db5fe6279c459ac2e71cceb12aaa35398ff0
wegoo <zhenwei1108@163.com>
2024-03-04 23:38:19 +0800

    update test demo

--- example/cert_test.go
+++ example/cert_test.go
@@ -15,7 +15,7 @@ import (
 
 func Test_code_cert(t *testing.T) {
 	var cert = "MIIB8zCCAZmgAwIBAgIKAKjKSNY55JbBcTAKBggqhkjOPQQDAjBCMSowKAYDVQQDDCFNaWRlYSBHcm91cCBNYXR0ZXIgUEFJIEcxIEkxIFByb2QxFDASBgorBgEEAYKifAIBEwQxMThDMCAXDTIzMTAyNDE1NTM0MVoYDzIwODcwODI0MTU1OTU5WjBXMSkwJwYDVQQDDCBEOUIyQUFFODcxQjUzRUU2NTYzMjQyOTYxNDUxRUJGRjEUMBIGCisGAQQBgqJ8AgETBDExOEMxFDASBgorBgEEAYKifAICEwQyMDIyMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEzFklDsljVKzMwNtBMBHLsx+psYLFYPMiGKKOTq2ur0S1SpcPQTmg0kUnu9X6RfJSnW7qBUJQDsPYXTwRifkv+qNgMF4wDAYDVR0TAQH/BAIwADAOBgNVHQ8BAf8EBAMCB4AwHQYDVR0OBBYEFFLhCG3CvDaY1Mu1QtyX2W783wfvMB8GA1UdIwQYMBaAFFm1h6KGoZDqlB20oJiuFdaAAxF4MAoGCCqGSM49BAMCA0gAMEUCIHdFM1UodAqZ73jmDrt786QEn3+jZ0lA7KZy7RzpBJiDAiEA7PRZ0D3NJfq7kKchqww8fDkWGIPwSZ6t5ADlwyuWC5g="
-	cert = "MIIBPDCB4qADAgECAgMB4kAwCgYIKoEcz1UBg3UwITELMAkGA1UEBhMCQ04xEjAQBgNVBAMTCVRlc3QgUm9vdDAeFw0yNDAyMTcxMDUwNDdaFw0yNjAyMTcxMDUwNDdaMBwxCzAJBgNVBAYTAkNOMQ0wCwYDVQQDEwRhZHNmMFMwDQYJKoEcz1UBgi0BBQADQgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKMUMBIwEAYDVR0TBAl0ZXN0IGNlcnQwCwYJKoEcz1UBgi0BBEgwRgIhAPVjH01NRzed8rBnMy1OODsTrvidOWCBOlY2ckSaCxi"
+	cert = "MIIBQjCB6qADAgECAgMB4kAwCgYIKoEcz1UBg3UwITELMAkGA1UEBhMCQ04xEjAQBgNVBAMTCVRlc3QgUm9vdDAmFxEyNDAzMDQyMzM3MjMrMDgwMBcRMjYwMzA0MjMzNzIzKzA4MDAwHDELMAkGA1UEBhMCQ04xDTALBgNVBAMTBGFkc2YwUzANBgkqgRzPVQGCLQEFAANCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAoxQwEjAQBgNVHRMECXRlc3QgY2VydDAKBggqgRzPVQGDdQNHADBEAiBccVBSujA4p1Y7gzBnBfGPKwjUFSTZ8FeoJHwyjnetfAIgXHFQUrowOKdWO4MwZwXxjysI1BUk2fBXqCR8Mo53rXw="
 	decodeBytes, err := base64.StdEncoding.DecodeString(cert)
 	if err != nil {
 		fmt.Println(err)
@@ -32,7 +32,7 @@ func Test_code_cert(t *testing.T) {
 
 func Test_create_cert(t *testing.T) {
 	//sm2
-	alg := pkix.AlgorithmIdentifier{Algorithm: asn1.ObjectIdentifier{1, 2, 156, 10197, 1, 301, 1}}
+	//alg := pkix.AlgorithmIdentifier{Algorithm: asn1.ObjectIdentifier{1, 2, 156, 10197, 1, 301, 1}}
 	SubjectName := pkix.Name{Country: []string{"CN"}, CommonName: "adsf"}.ToRDNSequence()
 	IssuerName := pkix.Name{Country: []string{"CN"}, CommonName: "Test Root"}.ToRDNSequence()
 	notBefore := time.Now()
@@ -47,6 +47,7 @@ func Test_create_cert(t *testing.T) {
 		Bytes:      realVersion,
 		IsCompound: true,
 	}
+	//todo 这个算法标识必须和下面一致
 	signAlg := pkix.AlgorithmIdentifier{Algorithm: asn1.ObjectIdentifier{1, 2, 156, 10197, 1, 501}}
 	id := asn1.ObjectIdentifier{2, 5, 29, 19}
 	extension := pkix.Extension{Id: id, Value: []byte("test cert")}
@@ -59,8 +60,8 @@ func Test_create_cert(t *testing.T) {
 	bigInt := new(big.Int).SetBytes(randomData)
 	signature := pkcs.SM2Signature{R: bigInt, S: bigInt}
 	signatureData, _ := asn1.Marshal(signature)
-
-	certificate := pkcs.Certificate{tbs, alg, asn1.BitString{Bytes: signatureData}}
+	//todo 这个算法标识必须和上面一致
+	certificate := pkcs.Certificate{tbs, signAlg, asn1.BitString{Bytes: signatureData}}
 	fmt.Println(certificate)
 	certBytes, err := asn1.Marshal(certificate)
 	fmt.Println(err)
