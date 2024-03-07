b61c96b7225e5f5877224db6bdfe88d8bff31c61
wegoo <zhenwei1108@163.com>
2024-03-07 23:17:06 +0800

    add alg oid

--- x509/base.go
+++ x509/base.go
@@ -6,6 +6,13 @@ import (
 	"math/big"
 )
 
+const (
+	SHA256WithRSA  = "1.2.840.113549.1.1.11"
+	SHA1WithRSA    = "1.2.840.113549.1.1.5"
+	SM3WithSM2     = "1.2.156.10197.1.501"
+	SHA256WithWapi = "1.2.156.11235.1.1.1"
+)
+
 type IssuerAndSerialNumber struct {
 	X500Name         asn1.RawValue "X500Name, 用于描述DN"
 	CertSerialNumber *big.Int
