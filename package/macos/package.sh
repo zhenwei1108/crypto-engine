652c7ac97c628d0efa768d4e80e091d41e3bbdff
wegoo <zhenwei1108@163.com>
2024-04-14 00:37:55 +0800

    增加打包脚本

--- package/macos/package.sh
+++ package/macos/package.sh
@@ -1,11 +1,22 @@
 #! /bin/bash
 
+###  参数定义
+# 应用名称
 APP_NAME=CertViewer
-
+# 编译后服务包目录
 Source_Path=/Users/$USER/$APP_NAME/app
+# 打包后,安装包目录
 Target_Path=/Users/$USER/$APP_NAME/result
+# 脚本目录
 Scripts_Path=/Users/$USER/$APP_NAME/scripts
-echo 
+# 源代码目录
+Code_Path=/Users/$USER/code-space/GolandProjects/learn/crypto-engine
+
+
+cd $Code_Path
+go build -o $Source_Path/$APP_NAME
+
+echo "完成代码编译"
 
 mkdir -p $Target_Path
 cd $Target_Path
