d2dcc058894a0175d183226846fa13e5ab0a7e62
zhenwei1108 <zhenwei1108@163.com>
2024-04-16 22:55:58 +0800

    完善脚本

--- package/macos/package.sh
+++ package/macos/package.sh
@@ -3,15 +3,27 @@
 ###  参数定义
 # 应用名称
 APP_NAME=CertViewer
+This_Path=$(cd $(dirname $0);pwd)
 # 编译后服务包目录
-Source_Path=/Users/$USER/$APP_NAME/app
+App_Path=$This_Path/$APP_NAME/app
 # 打包后,安装包目录
-Target_Path=/Users/$USER/$APP_NAME/result
+Target_Path=$This_Path/$APP_NAME//result
 # 脚本目录
-Scripts_Path=/Users/$USER/$APP_NAME/scripts
+Scripts_Path=$This_Path/$APP_NAME//scripts
 # 源代码目录
-Code_Path=/Users/$USER/code-space/GolandProjects/learn/crypto-engine
+Code_Path=$This_Path/../../
 
+
+echo 脚本当前目录:$This_Path
+echo 打包后应用目录:$App_Path
+echo 源代码所在目录:$Code_Path
+echo 源代码打包后服务目录:$Target_Path
+echo 应用安装前后执行的脚本目录:$Scripts_Path
+
+
+echo ----------
+
+echo 开始编译源代码
 
 cd $Code_Path
 go build -o $Source_Path/$APP_NAME
@@ -22,9 +34,10 @@ mkdir -p $Target_Path $Scripts_Path
 cd $Target_Path
 rm -f *.pkg
 
-echo remove  $Target_Path/$APP_NAME.pkg
+echo remove $Target_Path/$APP_NAME.pkg
 
-pkgbuild --root $Source_Path --install-location /Applications/$APP_NAME/Contents/ --identifier com.github.wegoo.$APP_NAME --version 1.0 --scripts $Scripts_Path  $Target_Path/$APP_NAME.pkg
+echo 开始构建应用
+pkgbuild --root $App_Path --install-location /Applications/$APP_NAME/Contents/ --identifier com.github.wegoo.$APP_NAME --version 1.0 --scripts $Scripts_Path  $Target_Path/$APP_NAME.pkg
 
 
-echo "打包完成 查看: " $Source_Path/$APP_NAME.pkg
+echo "完成 查看: " $App_Path/$APP_NAME.pkg
